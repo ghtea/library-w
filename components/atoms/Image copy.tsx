cebceaf039a106852542adc3ebd33e6eb80e3f85
jaehyeon(wiz) <eiirwp@gmail.com>
2021-08-19 00:48:37 +0900

    WIP add Image component using react-lazy-load-image-component

---
+++ components/atoms/Image copy.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect,useRef, useState } from "react";
+import { LazyLoadImage, LazyLoadImageProps } from 'react-lazy-load-image-component';
+
+// https://www.npmjs.com/package/react-lazy-load-image-component
+
+
+
+export type ImageProps = LazyLoadImageProps
+
+export const Image: React.FunctionComponent<ImageProps> = ({
+  src, alt, width = "auto", height = "auto"
+}) => {
+
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [isLoad, setIsLoad] = useState(false);
+
+  return (
+    <LazyLoadImage
+      alt={alt}
+      height={height}
+      src={src}
+      width={width} 
+    />
+  );
+}
