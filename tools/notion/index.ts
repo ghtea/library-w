0dadde055a775c0bbb8ef2cbf73ab0cf158fc9a9
jaehyeon(wiz) <eiirwp@gmail.com>
2021-08-25 22:48:55 +0900

    remove unnecessary files

--- tools/notion/index.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "@notionhq/client"
-
-export const notion = new Client({ auth: process.env.NOTION_KEY })
-
-export const notionFileUrlPrefix = process.env.NEXT_PUBLIC_NOTION_FILE_URL_PREFIX || "";
