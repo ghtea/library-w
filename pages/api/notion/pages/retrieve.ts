1afc67ce56334fae00bb02570413f37620b412fd
jaehyeon(wiz) <eiirwp@gmail.com>
2021-09-05 20:48:30 +0900

    add more notion api

--- pages/api/notion/pages/retrieve.ts
+++ pages/api/notion/pages/retrieve.ts
@@ -2,5 +2,5 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {notion} from "utils/notion";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return notion.pages.update(req.body)
+  return notion.pages.retrieve(req.body)
 }
